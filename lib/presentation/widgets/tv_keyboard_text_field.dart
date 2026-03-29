bbdc72100dc430e2212addc92ed5e43c774bbc49
Rawad Jahshan <70718218+RawadJahshan@users.noreply.github.com>
2026-03-29 15:19:21 +0300

    Fix TV text field focus highlight and arrow traversal

--- lib/presentation/widgets/tv_keyboard_text_field.dart
+++ lib/presentation/widgets/tv_keyboard_text_field.dart
@@ -22,7 +22,8 @@ class TvKeyboardTextField extends StatefulWidget {
 }
 
 class _TvKeyboardTextFieldState extends State<TvKeyboardTextField> {
-  final FocusNode _focusNode = FocusNode();
+  final FocusNode _traversalFocusNode = FocusNode(debugLabel: 'tvTextTraversal');
+  final FocusNode _textFieldFocusNode = FocusNode(debugLabel: 'tvTextEditor');
   bool _isEditing = false;
 
   static const Set<LogicalKeyboardKey> _activationKeys = <LogicalKeyboardKey>{
@@ -35,28 +36,70 @@ class _TvKeyboardTextFieldState extends State<TvKeyboardTextField> {
   @override
   void initState() {
     super.initState();
-    _focusNode.addListener(() {
-      if (!_focusNode.hasFocus && _isEditing) {
-        setState(() => _isEditing = false);
-      }
-    });
+    _textFieldFocusNode.canRequestFocus = false;
+
+    _traversalFocusNode.addListener(_onFocusChange);
+    _textFieldFocusNode.addListener(_onTextFieldFocusChange);
+  }
+
+  void _onFocusChange() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void _onTextFieldFocusChange() {
+    _onFocusChange();
+    if (!_textFieldFocusNode.hasFocus && _isEditing) {
+      _stopEditing();
+    }
   }
 
   @override
   void dispose() {
-    _focusNode.dispose();
+    _traversalFocusNode
+      ..removeListener(_onFocusChange)
+      ..dispose();
+    _textFieldFocusNode
+      ..removeListener(_onTextFieldFocusChange)
+      ..dispose();
     super.dispose();
   }
 
-  KeyEventResult _handleKeyEvent(FocusNode node, KeyEvent event) {
+  void _startEditing() {
+    if (_isEditing) {
+      return;
+    }
+
+    setState(() {
+      _isEditing = true;
+      _textFieldFocusNode.canRequestFocus = true;
+    });
+
+    _textFieldFocusNode.requestFocus();
+    SystemChannels.textInput.invokeMethod<void>('TextInput.show');
+  }
+
+  void _stopEditing() {
+    if (!_isEditing) {
+      return;
+    }
+
+    setState(() {
+      _isEditing = false;
+      _textFieldFocusNode.canRequestFocus = false;
+    });
+
+    _traversalFocusNode.requestFocus();
+  }
+
+  KeyEventResult _handleTraversalKeyEvent(FocusNode node, KeyEvent event) {
     if (event is! KeyDownEvent || _isEditing) {
       return KeyEventResult.ignored;
     }
 
     if (_activationKeys.contains(event.logicalKey)) {
-      setState(() => _isEditing = true);
-      _focusNode.requestFocus();
-      SystemChannels.textInput.invokeMethod<void>('TextInput.show');
+      _startEditing();
       return KeyEventResult.handled;
     }
 
@@ -66,23 +109,51 @@ class _TvKeyboardTextFieldState extends State<TvKeyboardTextField> {
   @override
   Widget build(BuildContext context) {
     return Focus(
-      onKeyEvent: _handleKeyEvent,
-      child: TextField(
-        focusNode: _focusNode,
-        controller: widget.controller,
-        readOnly: !_isEditing,
-        showCursor: _isEditing,
-        obscureText: widget.obscureText,
-        style: widget.style,
-        onTap: () {
-          if (!_isEditing) {
-            setState(() => _isEditing = true);
-          }
+      focusNode: _traversalFocusNode,
+      onKeyEvent: _handleTraversalKeyEvent,
+      child: Builder(
+        builder: (context) {
+          final hasFocus = _traversalFocusNode.hasFocus || _textFieldFocusNode.hasFocus;
+          final decoration = widget.decoration.copyWith(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(
+                color: hasFocus ? Colors.white : Colors.transparent,
+                width: hasFocus ? 2 : 1,
+              ),
+            ),
+          );
+
+          return AnimatedContainer(
+            duration: const Duration(milliseconds: 120),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: hasFocus
+                  ? const [
+                      BoxShadow(
+                        color: Color(0x55FFFFFF),
+                        blurRadius: 8,
+                        spreadRadius: 1,
+                      ),
+                    ]
+                  : null,
+            ),
+            child: TextField(
+              focusNode: _textFieldFocusNode,
+              controller: widget.controller,
+              readOnly: !_isEditing,
+              canRequestFocus: _isEditing,
+              showCursor: _isEditing,
+              obscureText: widget.obscureText,
+              style: widget.style,
+              onTap: _startEditing,
+              onChanged: widget.onChanged,
+              onSubmitted: (_) => _stopEditing(),
+              onEditingComplete: _stopEditing,
+              decoration: decoration,
+            ),
+          );
         },
-        onChanged: widget.onChanged,
-        onSubmitted: (_) => setState(() => _isEditing = false),
-        onEditingComplete: () => setState(() => _isEditing = false),
-        decoration: widget.decoration,
       ),
     );
   }
